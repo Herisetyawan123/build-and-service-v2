eb82df627f099d6faba87e4434c41ca973fa2ca5
Heri Setyawan <herisetyawan233@gmail.com>
2024-05-04 22:11:57 +0700

    fix: error session calling but not using

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={poppins.className + ' relative'}>
-        <Provider>{children}</Provider>
+        {children}
       </body>
     </html>
   );
